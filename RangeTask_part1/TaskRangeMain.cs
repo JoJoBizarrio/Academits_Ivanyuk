869797ef8dd551de6bf709fbcbff7be304f57b33
Константин <kon.ivanyuk@gmail.com>
2022-01-22 16:58:05 +0700

    - исправление имени солюшена - добавление задачи range

---
+++ RangeTask_part1/TaskRangeMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Range
+{
+    class TaskRangeMain
+    {
+        static void Main(string[] args)
+        {
+            Range range = new Range(-53.21, 21.98);
+            Console.WriteLine(range.GetLength());
+
+            range.From = 0.11;
+            Console.WriteLine(range.GetLength());
+
+            Console.WriteLine(range.IsInside(100.0));
+            Console.WriteLine(range.IsInside(20.121));
+        }
+    }
+}

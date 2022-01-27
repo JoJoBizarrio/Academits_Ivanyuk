230b8aae28397e0556d335079642748ad5dcab75
Константин <kon.ivanyuk@gmail.com>
2022-01-27 11:38:57 +0700

    Изменения: - тесты для заданий 1, 2, 3 объеденениы в один блок.

--- VectorTask/VectorTaskMain.cs
+++ VectorTask/VectorTaskMain.cs
@@ -10,32 +10,37 @@ namespace VectorTask
     {
         static void Main(string[] args)
         {
-            Console.Write("Check 1a: ");
+            // Блок 1, 2 и 3.
+            Console.Write("Check 1a, 2, 3: ");
             Vector vector1A = new Vector(9);
-            Console.Write(vector1A.ToString());
-            Console.WriteLine(".....dim: " + vector1A.GetSize());
+            Console.WriteLine(vector1A.ToString());
+            Console.WriteLine("Dimension: " + vector1A.GetSize());
             Console.WriteLine();
 
             // 1b after  1c
 
-            Console.Write("Check 1c: ");
+            Console.Write("Check 1c, 2, 3: ");
             double[] array1C = { 0.1, 9.2, 2.9 };
             Vector vector1C = new Vector(array1C);
-            Console.Write(vector1C.ToString());
-            Console.WriteLine(".....dim: " + vector1C.GetSize());
+            Console.WriteLine(vector1C.ToString());
+            Console.WriteLine("Dimension: " + vector1C.GetSize());
             Console.WriteLine();
 
-            Console.Write("Check 1b: ");
+            Console.Write("Check 1b, 2, 3: ");
             Vector vector1B = new Vector(vector1C);
-            Console.Write(vector1B.ToString());
-            Console.WriteLine(".....dim: " + vector1B.GetSize());
+            Console.WriteLine(vector1B.ToString());
+            Console.WriteLine("Dimension: " + vector1B.GetSize());
             Console.WriteLine();
 
-            Console.Write("Check 1d: ");
+            Console.Write("Check 1d, 2, 3: ");
             double[] array1D = { 1.1, 9.2, 2.9 };
             Vector vector1D = new Vector(5, array1D);
-            Console.Write(vector1D.ToString());
-            Console.WriteLine(".....dim: " + vector1D.GetSize());
+            Console.WriteLine(vector1D.ToString());
+            Console.WriteLine("Dimension: " + vector1D.GetSize());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            
 
         }
     }

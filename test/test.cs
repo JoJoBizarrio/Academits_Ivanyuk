c2b9aec68a281e70430ae453f701548306be9c21
Константин <kon.ivanyuk@gmail.com>
2022-02-05 20:52:43 +0700

    - +test project

---
+++ test/test.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+double[] array1 = { 1, 2 };
+double[,] array2 = { { 5, 2, 4 }, { 2, 3, 4 } };
+
+Console.WriteLine(array1.GetLength(0));
+Console.WriteLine(array1.Rank);
+Console.WriteLine(array2.GetLength(0));
+Console.WriteLine(array2.GetLength(1));
+Console.WriteLine(array2.Rank);
+
+double[,,] array3 = { { 1 }, { 2 } };

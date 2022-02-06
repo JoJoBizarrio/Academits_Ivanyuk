db62dfad89ef957ddbb7685b690386f0eea143f0
Константин <kon.ivanyuk@gmail.com>
2022-02-06 22:33:52 +0700

    - Квадрат и треугольник реализовали методы интерфейса

---
+++ IShapeTask/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IShapeTask
+{
+    internal class Square : IShape
+    {
+        internal double _sideSize;
+
+        public Square(double sideSize)
+        {
+            _sideSize = sideSize;
+        }
+
+        public double GetWidth()
+        {
+            return _sideSize;
+        }
+
+        public double GetHeight()
+        {
+            return GetWidth();
+        }
+
+        public double GetArea()
+        {
+            return _sideSize * _sideSize;
+        }
+
+        public double GetPerimeter()
+        {
+            return 4 * _sideSize;
+        }
+    }
+}
---
+++ IShapeTask/Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IShapeTask
+{
+    internal class Triangle : IShape
+    {
+        internal double X1;
+        internal double Y1;
+
+        internal double X2;
+        internal double Y2;
+
+        internal double X3;
+        internal double Y3;
+
+        public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+            X3 = x3;
+            Y3 = y3;
+        }
+
+        public double GetWidth()
+        {
+            return Math.Max(Math.Max(X1, X2), X3) - Math.Min(Math.Min(X1, X2), X3);
+        }
+
+        public double GetHeigth()
+        {
+            return Math.Max(Math.Max(Y1, Y2), Y3) - Math.Min(Math.Min(Y1, Y2), Y3);
+        }
+
+        public double GetArea()
+        {
+            return 0.5 * Math.Abs((X2 - X1) * (Y3 - Y1) - (X3 - X1) * (Y2 - Y1));
+        }
+
+        public double GetPerimetr()
+        {
+            return Math.Sqrt((X1 - X2) * (X1 - X2) + (Y1 - Y2) * (Y1 - Y2)) + Math.Sqrt((X2 - X3) * (X2 - X3) + (Y2 - Y3) * (Y2 - Y3) + Math.Sqrt((X3 - X1) * (X3 - X1) + (Y3 - Y1) * (Y3 - Y1)));
+        }
+    }
+}

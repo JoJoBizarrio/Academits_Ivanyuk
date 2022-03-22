04926b2023555e844163043635e6c6e9e8119ab6
Константин <kon.ivanyuk@gmail.com>
2022-03-22 12:16:20 +0700

    -

--- MatrixTask/MatrixTaskMain.cs
+++ MatrixTask/MatrixTaskMain.cs
@@ -9,7 +9,7 @@ namespace MatrixTask
             // 1c. Matrix(double[][]) – из двумерного массива(в C# double[,])
             double[,] array1 = { { 2, 3 }, { 4, 5 } };
             Matrix matrix1 = new Matrix(array1);
-            Console.Write(matrix1);
+            Console.WriteLine(matrix1);
 
             // 1d. Matrix(Vector[]) – из массива векторов-строк
             Vector[] vectorsArray = new Vector[2];

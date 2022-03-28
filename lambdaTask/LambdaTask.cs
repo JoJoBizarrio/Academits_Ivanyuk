f984b4606ec2d57ba479dfdf45d86bef859ddbed
Константин <kon.ivanyuk@gmail.com>
2022-03-28 23:56:48 +0700

    + Lambda

--- lambdaTask/LambdaTask.cs
+++ lambdaTask/LambdaTask.cs
@@ -27,15 +27,14 @@ namespace LambdaTask
             Person person1 = new("Misha", 15);
             Person person2 = new("Sasha", 20);
             Person person3 = new("Lex", 17);
-            Person person4 = new("Lutor", 49);
+            Person person4 = new("Lutor", 44);
             Person person5 = new("Saitama", 25);
             Person person6 = new("Misha", 23);
             Person person7 = new("Sasha", 76);
 
             List<Person> people = new() { person1, person2, person3, person4, person5, person6, person7 };
 
-            //А) получить список уникальных имен
-            //Б) вывести список уникальных имен в формате:
+            //А) получить список уникальных имен + Б) вывести список уникальных имен в формате:
             var peopleNames = people.Select(x => x.Name);
 
             Console.WriteLine("Имена: " + String.Join(", ", peopleNames.Distinct()));
@@ -53,7 +52,9 @@ namespace LambdaTask
             Console.WriteLine(String.Join(", ", personsByName));
 
             //Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
+            var peopleBetween20and45 = people.Where(x => x.Age >= 20 && x.Age <= 45 ).OrderByDescending(x => x.Age);
 
+            Console.WriteLine(String.Join(", ", peopleBetween20and45));
         }
     }
 }

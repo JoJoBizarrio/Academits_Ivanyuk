47c48e74094351cd1bf21926583403fd591291ce
Константин <kon.ivanyuk@gmail.com>
2022-03-28 17:38:38 +0700

    + LambdaTask

--- lambdaTask/LambdaTask.cs
+++ lambdaTask/LambdaTask.cs
@@ -24,32 +24,6 @@ namespace LambdaTask
 
         static void Main(string[] randomName)
         {
-
-
-            //А) получить список уникальных имен
-            //List<Person> unicumName = new List<Person>();
-            //Predicate<Person> f = delegate (Person x)
-            //{
-            //    for (int i = 0; i < people.Count; ++i)
-            //    {
-            //        try
-            //        {
-            //            if (x.Name == unicumName[i].Name)
-            //            {
-            //                return false;
-            //            }
-            //        }
-            //        catch
-            //        {
-            //            continue;
-            //        }
-            //    }
-
-            //    return true;
-            //};
-            //unicumName = Filter(people, f);
-            //Console.WriteLine(String.Join(", ", unicumName));
-
             Person person1 = new("Misha", 15);
             Person person2 = new("Sasha", 20);
             Person person3 = new("Lex", 17);
@@ -60,22 +34,26 @@ namespace LambdaTask
 
             List<Person> people = new() { person1, person2, person3, person4, person5, person6, person7 };
 
+            //А) получить список уникальных имен
+            //Б) вывести список уникальных имен в формате:
             var peopleNames = people.Select(x => x.Name);
 
-            Console.WriteLine(String.Join(", ", peopleNames.Distinct()));
+            Console.WriteLine("Имена: " + String.Join(", ", peopleNames.Distinct()));
 
-            //•
-            //Б) вывести список уникальных имен в формате:
-            //Имена: Иван, Сергей, Петр.
-            //•
-            //В) получить список людей младше 18, посчитать для них средний
-            //возраст
-            //•
-            //Г) при помощи группировки получить Map , в котором ключи
-            //имена, а значения средний возраст
-            //•
-            //Д) получить людей, возраст которых от 20 до 45, вывести в консоль
-            //их имена в порядке убывания возраста
+            //В) получить список людей младше 18, посчитать для них средний возраст
+
+            var peopleLess18 = people.Where(x => x.Age < 18).Select(x => x);
+            var average = peopleLess18.Average(x => x.Age);
+
+            Console.WriteLine($"Имена: {String.Join(", ", peopleLess18) } \tСредний возраст: {average}");
+
+            //Г) при помощи группировки получить Map , в котором ключи имена, а значения средний возраст
+            Dictionary<string, List<Person>> personsByName = people.GroupBy(x => x.Name).ToDictionary(x => x.Key, x => x.ToList());
+
+            Console.WriteLine(String.Join(", ", personsByName));
+
+            //Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
+
         }
     }
 }

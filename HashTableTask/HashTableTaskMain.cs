02a52975e44fb501ced94cb0a0e0a73c68e4be3a
Константин <kon.ivanyuk@gmail.com>
2022-04-01 22:02:52 +0700

    закомментил main

--- HashTableTask/HashTableTaskMain.cs
+++ HashTableTask/HashTableTaskMain.cs
@@ -4,33 +4,33 @@
     {
         static void Main(string[] args)
         {
-            HashTable<int> ht = new HashTable<int>(100);
+            //HashTable<int> ht = new HashTable<int>(100);
 
-            ht.Add(0);
-            ht.Add(16);
-            ht.Add(124);
-            ht.Add(24);
+            //ht.Add(0);
+            //ht.Add(16);
+            //ht.Add(124);
+            //ht.Add(24);
 
-            Console.WriteLine(ht);
+            //Console.WriteLine(ht);
 
-            int[] array1 = new int[40];
+            //int[] array1 = new int[40];
 
-            ht.CopyTo(array1, 3);
+            //ht.CopyTo(array1, 3);
 
-            Console.WriteLine(string.Join(", ", array1));
-            Console.WriteLine();
+            //Console.WriteLine(string.Join(", ", array1));
+            //Console.WriteLine();
 
-            foreach (int e in ht)
-            {
-                Console.Write($"{e}; ");
-            }
+            //foreach (int e in ht)
+            //{
+            //    Console.Write($"{e}; ");
+            //}
 
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            ht.Contains(0);
-            ht.Remove(124);
-            Console.WriteLine(ht);
+            //ht.Contains(0);
+            //ht.Remove(124);
+            //Console.WriteLine(ht);
         } 
     }
 }

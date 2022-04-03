d6a517a717b8f6929b8582e3d9e5d94a211a9471
Константин <kon.ivanyuk@gmail.com>
2022-04-03 13:40:59 +0700

    +) 1. В коде не должно быть 2 и более пустых строк подряд
    +) 2. _from, _to - здесь вместо приватных полей должны быть автосвойства, как было в прошлой версии.
    Сейчас значения полей нельзя получить и изменить
    
    +) 3. GetSum - объединение принято обозначать словом Union
    
    +) 4. http://joxi.ru/KAxKoRPCVZLok2 - в таких случаях объявление массива лучше форматировать так: http://joxi.ru/GrqEX6PHz4KWnr
    
    5. В разности не нужно отдельно рассматривать случай ==.
    Этот случай должен входить в другой, более общий случай
    
    +) 6.  new Range[] { }; - пустой массив можно создать так:  new Range[0];
    
    +) 7. ToString(Range[] range):
    - в имени аргумента не хватает множественного числа
    - списки нужно печатать через запятую
    - при печати массива диапазонов лучше печатать диапазоны внутри квадратных скобок [], означающих массив, чтобы получалось так:
    [(1; 3), (5; 7)] - для двух диапазонов в массиве
    [(1; 7)] - для одного диапазона в массиве
    [] - для пустого массива
    
    +) 8. В файлах нужно удалять неиспользуемые using'и
    
    9. В пересечении по одной точке пусть считается, что пересечения нет (это нужно, чтобы результаты выглядели более логично)
    
    +) 10. Есть ошибки в вычислении разности:
    (1; 3) - (3; 5) = [(1; 3), (3; 5)] - а должно быть: [(1; 3)]
    (3; 5) - (1; 3) = [(3; 1), (5; 3)] - а должно быть: [(3; 5)]
    (1; 5) - (3; 7) = [(1; 3), (5; 7)] - а должно быть: [(1; 3)]
    (3; 7) - (1; 5) = [(3; 1), (7; 5)] - а должно быть: [(5; 7)]
    (1; 3) - (1; 5) = [(1; 3)] - а должно быть: []
    (1; 5) - (1; 3) = [(1; 3)] - а должно быть: [(3; 5)]
    (3; 7) - (5; 7) = [(5; 7)] - а должно быть: [(3; 5)]
    (5; 7) - (3; 7) = [(5; 7)] - а должно быть: []
    (1; 7) - (1; 7) = [(1; 7)] - а должно быть: []
    (1; 7) - (3; 5) = [(1; 3), (7; 5)] - а должно быть: [(1; 3), (5; 7)]
    
    +) 11. if (stringBuilder.ToString() == string.Empty)
    Проверять на пустую строку лучше через длину или через метод string.IsNullOrEmpty

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -4,94 +4,121 @@ namespace RangeTask
 {
     public class Range
     {
-        private double _from;
+        public double From { get; set; }
 
-        private double _to;
+        public double To { get; set; }
 
         // вместо {4. Сделать метод для получения длины диапазона}, свойство:
-        public double Length => _to - _from;
+        public double Length => To - From;
+
+        public Range() { }
 
         // 2. Описать конструктор, при помощи которого заполняются поля
         public Range(double from, double to)
         {
-            _from = from;
-            _to = to;
+            From = from;
+            To = to;
         }
 
         // 5. Сделать метод isInside, который принимает вещественное число и возвращает boolean – результат проверки, принадлежит ли число диапазону: 
         public bool IsInside(double number)
         {
-            return number >= _from && number <= _to;
+            return number >= From && number <= To;
         }
-
-       
 
         // Получение интервала-пересечения двух интервалов. Если пересечения нет, выдать null. Если есть, то выдать новый диапазон с соответствующими концами
         public Range GetIntersection(Range range)
         {
-            if (_to < range._from || _from > range._to)
+            if (To < range.From || From > range.To)
             {
-                return null;
+                return new Range();
             }
 
-            return new Range(Math.Max(_from, range._from), Math.Min(_to, range._to));
+            return new Range(Math.Max(From, range.From), Math.Min(To, range.To));
         }
 
         // Получение объединения двух интервалов. Может получиться 1 или 2 отдельных куска.
-        public Range[] GetSum(Range range)
+        public Range[] GetUnion(Range range)
         {
-            if (_to < range._from || _from > range._to)
+            if (To < range.From || From > range.To)
             {
-                return new Range[]{
-                    new Range(_from, _to),
-                    new Range(range._from, range._to),
-                                  };
+                return new Range[]
+                {
+                    new Range(From, To),
+                    new Range(range.From, range.To),
+                };
             }
 
-            return new Range[] { new Range(Math.Min(_from, range._from), Math.Max(_to, range._to)) };
+            return new Range[] { new Range(Math.Min(From, range.From), Math.Max(To, range.To)) };
         }
 
         // Получение разности двух интервалов.
         public Range[] GetDifference(Range range)
         {
-            if (_to < range._from || _from > range._to)
+            if (To <= range.From || From >= range.To)
             {
-                return new Range[] { new Range(_from, _to) }; // что будет если использовать: return new Range[] { this }; ?
+                return new Range[] { new Range(From, To) };
             }
 
-            if (_from > range._from & _to < range._to)
+            if (From >= range.From & To <= range.To)
             {
-                return new Range[] { };
+                return new Range[default];
             }
 
-            if (_from == range._from || _to == range._to)
+            if (From == range.From)
             {
-                return new Range[] { new Range(Math.Max(_from, range._from), Math.Min(_to, range._to)) };
+                return new Range[] { new Range(range.To, To) };
             }
 
-            return new Range[] { new Range(_from, range._from), new Range(_to, range._to) };
+            if (To == range.To)
+            {
+                return new Range[] { new Range(From, range.From) };
+            }
+
+            if (IsInside(range.From) && !IsInside(range.To))
+            {
+                return new Range[] { new Range(From, range.From) };
+            }
+
+            if (!IsInside(range.From) && IsInside(range.To))
+            {
+                return new Range[] { new Range(range.To, To) };
+            }
+
+            return new Range[] { new Range(From, range.From), new Range(range.To, To) };
         }
 
         public override string ToString()
         {
-            return $"({_from}; {_to})";
+            if (From == 0 & To == 0)
+            {
+                return "[]";
+            }
+
+            return $"({From}; {To})";
         }
 
-        public static string ToString(Range[] range)
+        public static string ToString(Range[] ranges)
         {
-            StringBuilder stringBuilder = new();
+            StringBuilder stringBuilder = new("[");
 
-            foreach (Range e in range)
+            foreach (Range e in ranges)
             {
                 stringBuilder.Append(e);
+                stringBuilder.Append(", ");
             }
 
-            if (stringBuilder.ToString() == string.Empty)
+            if (string.IsNullOrEmpty(stringBuilder.ToString()))
             {
-                return "Lack of range";
+                return "[]";
             }
 
-            return stringBuilder.ToString();
+            if (stringBuilder.ToString() == "[")
+            {
+                return stringBuilder.Append(']').ToString();
+            }
+
+            return stringBuilder.Replace(", ", "]", stringBuilder.Length - 2, 2).ToString();
         }
     }
 }
--- RangeTask/RangeTaskMain.cs
+++ RangeTask/RangeTaskMain.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace RangeTask
+﻿namespace RangeTask
 {
     class RangeTaskMain
     {
@@ -21,27 +19,35 @@ namespace RangeTask
             Range range4 = new Range(-30, 20);  // левая граница снаружи 1-го диапазона
             Range range5 = new Range(0, 40);    // правая граница снаружи 1-го диапазона
             Range range6 = new Range(40, 50);   // не пересекаются
+            Range range7 = new Range(-10, 40);
+            Range range8 = new Range(-10, 10);
 
             Console.WriteLine();
-            Console.WriteLine(range1.GetIntersection(range3));
-            Console.WriteLine(range1.GetIntersection(range4));
-            Console.WriteLine(range1.GetIntersection(range5));
-            Console.WriteLine(range1.GetIntersection(range6));
+            Console.WriteLine("Check intersection:");
+            Console.WriteLine($"{range1} and {range3}: " + range1.GetIntersection(range3));
+            Console.WriteLine($"{range1} and {range4}: " + range1.GetIntersection(range4));
+            Console.WriteLine($"{range1} and {range5}: " + range1.GetIntersection(range5));
+            Console.WriteLine($"{range1} and {range6}: " + range1.GetIntersection(range6));
+            Console.WriteLine($"{range1} and {range7}: " + range1.GetIntersection(range7));
+            Console.WriteLine($"{range1} and {range8}: " + range1.GetIntersection(range8));
 
             Console.WriteLine();
             Console.WriteLine("Check sum:");
-            Console.WriteLine(Range.ToString(range1.GetSum(range2)));
-            Console.WriteLine(Range.ToString(range1.GetSum(range3)));
-            Console.WriteLine(Range.ToString(range1.GetSum(range4)));
-            Console.WriteLine(Range.ToString(range1.GetSum(range5)));
-            Console.WriteLine(Range.ToString(range1.GetSum(range6)));
+            Console.WriteLine($"{range1} and {range2}: " + Range.ToString(range1.GetUnion(range2)));
+            Console.WriteLine($"{range1} and {range3}: " + Range.ToString(range1.GetUnion(range3)));
+            Console.WriteLine($"{range1} and {range4}: " + Range.ToString(range1.GetUnion(range4)));
+            Console.WriteLine($"{range1} and {range5}: " + Range.ToString(range1.GetUnion(range5)));
+            Console.WriteLine($"{range1} and {range6}: " + Range.ToString(range1.GetUnion(range6)));
 
             Console.WriteLine();
             Console.WriteLine("Check difference:");
-            Console.WriteLine(Range.ToString(range1.GetDifference(range3)));
-            Console.WriteLine(Range.ToString(range1.GetDifference(range4)));
-            Console.WriteLine(Range.ToString(range1.GetDifference(range5)));
-            Console.WriteLine(Range.ToString(range1.GetDifference(range6)));
+            Console.WriteLine($"{range1} and {range2}: " + Range.ToString(range1.GetDifference(range2)));
+            Console.WriteLine($"{range1} and {range3}: " + Range.ToString(range1.GetDifference(range3)));
+            Console.WriteLine($"{range1} and {range4}: " + Range.ToString(range1.GetDifference(range4)));
+            Console.WriteLine($"{range1} and {range5}: " + Range.ToString(range1.GetDifference(range5)));
+            Console.WriteLine($"{range1} and {range6}: " + Range.ToString(range1.GetDifference(range6)));
+            Console.WriteLine($"{range1} and {range7}: " + Range.ToString(range1.GetDifference(range7)));
+            Console.WriteLine($"{range1} and {range8}: " + Range.ToString(range1.GetDifference(range8)));
         }
     }
 }

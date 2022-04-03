e50d6a2d1a099b87d1c956d322a07464d67662c7
Константин <kon.ivanyuk@gmail.com>
2022-04-03 19:56:01 +0700

    +

--- TemperatureTask/TemperatureTaskForm.cs
+++ TemperatureTask/TemperatureTaskForm.cs
@@ -37,26 +37,35 @@ namespace TemperatureTask
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(textBox1.Text))
+            if (radioButton1.Enabled)
             {
-                textBox1.Text = Convert.ToString(0);
-                textBox1.SelectionStart = 1;
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    textBox1.Text = Convert.ToString(0);
+                    textBox1.SelectionStart = 1;
+                }
+
+                //int index = textBox1.Text.LastIndexOf('.');
+
+                //if (index >= 0)
+                //{
+                //    textBox1.Text = textBox1.Text.Remove(index);
+                //    textBox1.SelectionStart = textBox1.TextLength;
+                //}
+
+                try // а тут надо это использовать? одну ошибку с нулем я обработал. но тут еще проблемы: с точкой/запятой, с несколькими запятыми. Все вручную обрабатывать чтоли? 
+                {
+                    double celsius = Convert.ToDouble(textBox1.Text);
+                    textBox4.Text = Convert.ToString(celsius + 273.15);
+                }
+                catch { MessageBox.Show("Something wrong..."); }
             }
 
-            //int index = textBox1.Text.LastIndexOf('.');
-
-            //if (index >= 0)
-            //{
-            //    textBox1.Text = textBox1.Text.Remove(index);
-            //    textBox1.SelectionStart = textBox1.TextLength;
-            //}
-
-            try
+            if (radioButton4.Enabled)
             {
-                double celsius = Convert.ToDouble(textBox1.Text);
-                textBox4.Text = Convert.ToString(celsius + 273.15);
+
             }
-            catch { MessageBox.Show("Something wrong..."); }
+            
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
@@ -73,5 +82,8 @@ namespace TemperatureTask
         {
 
         }
+
+
+
     }
 }

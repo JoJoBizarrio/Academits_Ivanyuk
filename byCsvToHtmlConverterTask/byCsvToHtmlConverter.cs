e0d48128c67164cf90ac810609ba9056d181af36
Константин <kon.ivanyuk@gmail.com>
2022-04-06 00:45:50 +0700

    + begun CsvToHtml

---
+++ byCsvToHtmlConverterTask/byCsvToHtmlConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace byCsvToHtmlConverterTask
+{
+    internal class ByCsvToHtmlConverter
+    {
+        public static void ConvertCsvToHtml(string CsvPath, string HtmlPath)
+        {
+            if (!File.Exists(CsvPath) || !File.Exists(HtmlPath))
+            {
+                Console.WriteLine("File(s) doesn't exist.");
+            }
+            else
+            {
+                using StreamReader reader = new StreamReader(CsvPath);
+                using StreamWriter writer = new StreamWriter(HtmlPath);
+
+                char[] currentRow;
+                char cellSeparator = ',';
+                char specificSymbolsDesignator = '"';
+                bool isCellStartWithRowBreak = false;
+                bool isCellContinuationWithRowBreak = false;
+                bool isCellEndWithRowBreak = false;
+
+                writer.WriteLine("<table>");
+
+                while ((currentRow = reader.ReadLine().ToCharArray()) != null)
+                {
+                    foreach (char e in currentRow)
+                    {
+
+                    }
+                }
+}

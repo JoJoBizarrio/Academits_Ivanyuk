1e839b01ca198eb2fc30a7803be5ea1452b07827
Константин <kon.ivanyuk@gmail.com>
2022-04-06 13:59:46 +0700

    Converter

--- byCsvToHtmlConverterTask/byCsvToHtmlConverter.cs
+++ byCsvToHtmlConverterTask/byCsvToHtmlConverter.cs
@@ -2,36 +2,130 @@
 {
     internal class ByCsvToHtmlConverter
     {
-        public static void ConvertCsvToHtml(string CsvPath, string HtmlPath)
+        public static void ConvertCsvToHtml(string csvPath, string htmlPath)
         {
-            if (!File.Exists(CsvPath) || !File.Exists(HtmlPath))
+            if (!File.Exists(csvPath))
             {
-                Console.WriteLine("File(s) doesn't exist.");
+                Console.WriteLine("File(s) .csv doesn't exist.");
             }
-            else
+
+            if (!File.Exists(htmlPath))
             {
-                using StreamReader reader = new StreamReader(CsvPath);
-                using StreamWriter writer = new StreamWriter(HtmlPath);
+                File.Create(htmlPath);
+            }
 
-                char[] currentRow;
+            using StreamReader reader = new StreamReader(csvPath);
+            using StreamWriter writer = new StreamWriter(htmlPath);
 
-                //bool isCellStartWithRowBreak = false;
-                //bool isCellContinuationWithRowBreak = false;
-                //bool isCellEndWithRowBreak = false;
+            char[] buffer = new char[1];
+            char[] bufferForSpecificSymbolsDesignator = new char[1];
+            char currentSymbol;
 
-                writer.WriteLine("<table>");
+            char cellSeparator = ',';
+            char specificSymbolsDesignator = '"';
+            bool isCellWithRowBreakOrSpecificSymbols = false;
+            //bool isCellStartWithRowBreak = false;
+            //bool isCellContinuationWithRowBreak = false;
+            //bool isCellEndWithRowBreak = false;
 
-                while ((currentRow = reader.ReadLine().ToCharArray()) != null)
+            writer.WriteLine("<!DOCTYPE html>");
+            writer.WriteLine("<html>");
+            writer.WriteLine("<head>");
+            writer.WriteLine("<meta charset = \"utf - 8\" >");
+            writer.WriteLine($"<title> {htmlPath} </title>");
+            writer.WriteLine("</head>");
+            writer.WriteLine("<body>");
+            writer.WriteLine("<p> </p>");
+            writer.WriteLine("</body>");
+            writer.WriteLine("</html>");
+
+            writer.WriteLine("<table>");
+
+            for (int i = 0; reader.Read(buffer, i, 1) >= 0; ++i)
+            {
+                currentSymbol = buffer[default];
+
+                if (isCellWithRowBreakOrSpecificSymbols)
                 {
-                    char cellSeparator = ',';
-                    char specificSymbolsDesignator = '"';
-
-                    foreach (char e in currentRow)
+                    if (i == 0)
                     {
-
+                        writer.Write("<br/>");
+                        writer.Write(currentSymbol);
+                        continue;
                     }
+
+                    if (currentSymbol == specificSymbolsDesignator)
+                    {
+                        if (reader.Read(bufferForSpecificSymbolsDesignator, i + 1, 1) < 0)
+                        {
+                            isCellWithRowBreakOrSpecificSymbols = false;
+                            continue;
+                        }
+
+                        if (bufferForSpecificSymbolsDesignator[0] == specificSymbolsDesignator)
+                        {
+                            writer.Write(currentSymbol);
+                            ++i;
+                            continue;
+                        }
+
+                        continue;
+                    }
+
+                    writer.Write(currentSymbol);
+                    continue;
                 }
+
+                if (i == 0)
+                {
+                    writer.Write("<td>");
+                    writer.Write(currentSymbol);
+                    continue;
+                }
+
+                if (currentSymbol == specificSymbolsDesignator)
+                {
+                    isCellWithRowBreakOrSpecificSymbols = true;
+                    continue;
+                }
+
+                if (currentSymbol == cellSeparator)
+                {
+                    writer.Write("</td> <td>");
+                    continue;
+                }
+
+                writer.Write(currentSymbol);
             }
+
+            if (!isCellWithRowBreakOrSpecificSymbols)
+            {
+                writer.WriteLine("/tr");
+            }
+
+            writer.WriteLine("/<table>");
+
         }
     }
 }
+
+//if (currentSymbol == specificSymbolsDesignator && reader.Read(bufferForSpecificSymbolsDesignator, i + 1, 1) >= 0)
+//{
+//    if (bufferForSpecificSymbolsDesignator[0] == specificSymbolsDesignator)
+//    {
+//        writer.Write(currentSymbol);
+//        ++i;
+//        continue;
+//    }
+//    else
+//    {
+//        isCellWithRowBreakOrSpecificSymbols = false;
+//        continue;
+//    }
+//}
+
+//if (currentSymbol == specificSymbolsDesignator)
+//{
+//    isCellWithRowBreakOrSpecificSymbols = false;
+//    break;
+//}

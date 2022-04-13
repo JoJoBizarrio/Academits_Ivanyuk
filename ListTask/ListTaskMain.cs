1fe7bb95790e7c4802c3304e2299f8178740e9b3
Константин <kon.ivanyuk@gmail.com>
2022-04-13 20:14:16 +0700

    =

--- ListTask/ListTaskMain.cs
+++ ListTask/ListTaskMain.cs
@@ -6,23 +6,23 @@ namespace ListTask
     {
         public static void Main(string[] args)
         {
-            SinglyLinkedList<int> list = new(1, 2, 3, 4, 5);
-            Console.WriteLine(list);
+            //SinglyLinkedList<int> list = new(1, 2, 3, 4, 5);
+            //Console.WriteLine(list);
 
-            list.RemoveAt(2);
-            Console.WriteLine(list);
-            list.Remove(5); 
-            Console.WriteLine(list);
+            //list.RemoveAt(2);
+            //Console.WriteLine(list);
+            //list.Remove(5); 
+            //Console.WriteLine(list);
 
-            list.Reverse();
-            Console.WriteLine(list);
+            //list.Reverse();
+            //Console.WriteLine(list);
 
-            SinglyLinkedList<string> stringsList = new SinglyLinkedList<string>("1", "2");
+            //SinglyLinkedList<string> stringsList = new SinglyLinkedList<string>("1", "2");
 
-            stringsList.SetData(0, null);
-            Console.WriteLine(stringsList);
-            stringsList.RemoveFirst();
-            Console.WriteLine(stringsList);
+            //stringsList.SetData(0, null);
+            //Console.WriteLine(stringsList);
+            //stringsList.RemoveFirst();
+            //Console.WriteLine(stringsList);
 
         }
     }

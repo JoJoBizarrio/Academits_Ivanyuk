f2940b8a1936ede04cf948d5189c45f6907bab04
Константин <kon.ivanyuk@gmail.com>
2022-04-23 18:16:05 +0700

    + Убрал лишнее

--- GraphTask/GraphTaskMain.cs
+++ GraphTask/GraphTaskMain.cs
@@ -4,17 +4,7 @@
     {
         static void Main()
         {
-            Graph graph = new(7);
 
-            graph.ConnectPeeks(0, 1);
-            graph.ConnectPeeks(1, 2);
-            graph.ConnectPeeks(1, 3);
-            graph.ConnectPeeks(1, 4);
-            graph.ConnectPeeks(1, 5);
-            graph.ConnectPeeks(2, 6);
-
-            Console.WriteLine(graph.BypassByDeep());
-            Console.WriteLine(graph.BypassByWidth());
         }
     }
 }

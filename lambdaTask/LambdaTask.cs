9dbec26351ad2f13df6ce820e803033a1913719e
Константин <kon.ivanyuk@gmail.com>
2022-04-27 23:35:21 +0700

    +) 1. namesFilter, peopleWithAgeLess18Filter, personsByNameFilter, peopleWithAgeBetween20And45Filter - имена неправильные, т.к. это не фильтры
    +) 2. namesFilter, peopleNames:
    - в текущем варианте лучше сделать, чтобы эти переменные отличались только словом List
    - в целом, первая переменная не нужна, можно сразу получить список, но сохранить в переменную peopleNames
    
    +) 3. Аналогичные замечания для других пар переменных.
    В целом, обычно не нужна пара переменных IEnumerable и List.
    Обычно должно быть либо то, либо другое.
    При этом, во многих случаях, в имени можно не указывать список это или IEnumerable, т.к. это не важно.
    Как правило, важнее какие данные хранит коллекция
    
    +) 4. average - неинформативное имя, по смыслу это средний возраст
    
    +) 5. http://joxi.ru/52aOz0jSlkeZbr - здесь нет необходимости в преобразовании в список
    
    +) 6. personsByNameFilter, personsByName - имена неправильные, т.к. эти переменные не хранят персоны, а хранят средний возраст по имени
    
    +) 7. people - лучше тоже переделать на использование var

--- lambdaTask/LambdaTask.cs
+++ lambdaTask/LambdaTask.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            List<Person> people = new()
+            var people = new List<Person>()
             {
                 new("Misha", 15),
                 new("Sasha", 20),
@@ -16,45 +16,42 @@
             };
 
             //А) получить список уникальных имен + Б) вывести список уникальных имен в формате:
-            var namesFilter = people
+            var peopleNames = people
                 .Select(x => x.Name)
-                .Distinct();
-
-            var peopleNames = namesFilter.ToList();
+                .Distinct()
+                .ToList();
 
             Console.WriteLine("Имена: " + string.Join(", ", peopleNames));
             Console.WriteLine();
 
             //В) получить список людей младше 18, посчитать для них средний возраст
-            var peopleWithAgeLess18Filter = people
-                .Where(x => x.Age < 18);
+            var peopleWithAgeLess18 = people
+                .Where(x => x.Age < 18)
+                .ToList();
 
-            var peopleWithAgeLess18 = peopleWithAgeLess18Filter.ToList();
-            var average = peopleWithAgeLess18.Average(x => x.Age);
+            var averageAge = peopleWithAgeLess18.Average(x => x.Age);
 
-            Console.WriteLine($"Имена: {string.Join(", ", peopleWithAgeLess18.Select(x => x.Name)) }\tСредний возраст: {average}");
+            Console.WriteLine($"Имена: {string.Join(", ", peopleWithAgeLess18.Select(x => x.Name)) }\tСредний возраст: {averageAge}");
             Console.WriteLine();
 
             //Г) при помощи группировки получить Map , в котором ключи имена, а значения средний возраст
-            var personsByNameFilter = people
+            var averageAgeByName = people
                 .GroupBy(x => x.Name)
                 .ToDictionary(x => x.Key, x => x
-                                                .ToList()
                                                 .Average(x => x.Age))
                 .ToList();
 
-            var personsByName = personsByNameFilter;
-
-            Console.WriteLine(string.Join(", ", personsByName));
+            Console.WriteLine(string.Join(", ", averageAgeByName));
             Console.WriteLine();
 
             //Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
-            var peopleWithAgeBetween20And45Filter = people
+            var peopleWithAgeBetween20And45 = people
                 .Where(x => x.Age >= 20 && x.Age <= 45)
                 .OrderByDescending(x => x.Age)
-                .Select(x => x.Name);
+                .Select(x => x.Name)
+                .ToList();
 
-            Console.WriteLine(string.Join(", ", peopleWithAgeBetween20And45Filter));
+            Console.WriteLine(string.Join(", ", peopleWithAgeBetween20And45));
         }
     }
 }

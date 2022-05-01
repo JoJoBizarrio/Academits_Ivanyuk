08fabfd4466212f63fa612eea7d9359fdf620dee
Константин <kon.ivanyuk@gmail.com>
2022-05-01 12:13:15 +0700

    +

--- GraphTask/GraphTaskMain.cs
+++ GraphTask/GraphTaskMain.cs
@@ -4,15 +4,15 @@
     {
         static void Main()
         {
-            Graph graph = new Graph(7, 0, 1, 2, 6, 5, 1, 4, 5, 1, 3);
+            //Graph graph = new Graph(7, 0, 1, 2, 6, 5, 1, 4, 5, 1, 3);
 
-            Action<int> action = (x) => { Console.Write(x); Console.Write(", "); };
+            //Action<int> action = (x) => { Console.Write(x); Console.Write(", "); };
 
-            graph.BypassByWidth(action);
-            Console.WriteLine();
-            graph.BypassByDeep(action);
-            Console.WriteLine();
-            graph.BypassByRecursiveDeep(action);
+            //graph.BypassByWidth(action);
+            //Console.WriteLine();
+            //graph.BypassByDeep(action);
+            //Console.WriteLine();
+            //graph.BypassByRecursiveDeep(action);
         }
     }
 }
